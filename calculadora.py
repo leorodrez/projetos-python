6d23a3246f386a59afc9b93d398b4e7ae0d1ca45
leorodrez <126107916+leorodrez@users.noreply.github.com>
2023-07-21 14:44:41 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,45 +1,45 @@
 while True:
-  numero_1 = input('Digite um número: ')
-  numero_2 = input('Digite outro número: ')
-  operador = input('Digite o operador (+-*/): ')
-  numeros_validos = None #flag para determinar uma condição ao longo do código
-  num_1_float = 0
-  num_2_float = 0
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite o operador (+-*/): ')
+    numeros_validos = None #flag para determinar uma condição ao longo do código
+    num_1_float = 0
+    num_2_float = 0
 
-  try:
-    num_1_float = float(numero_1)
-    num_2_float = float(numero_2)
-    numeros_validos = True
-  except:
-    numeros_validos = None
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
 
-  if numeros_validos is None:
-    print("Erro! Digite apenas números!")
-    continue
+    if numeros_validos is None:
+        print("Erro! Digite apenas números!")
+        continue
 
-  operadores_permitidos = '+-*/'
+    operadores_permitidos = '+-*/'
 
-  if operador not in operadores_permitidos:
-    print('Erro!Escolha apenas entre os quatro operadores mencionados')
-    continue
+    if operador not in operadores_permitidos:
+        print('Erro!Escolha apenas entre os quatro operadores mencionados')
+        continue
 
-  if len(operador) > 1:
-    print('Digite apenas um operador')
-    continue
+    if len(operador) > 1:
+        print('Digite apenas um operador')
+        continue
 
-  if operador == '+':
-    print(f'O resultado é: {num_1_float + num_2_float}')
-  elif operador == '-':
-    print(f'O resultado é: {num_1_float - num_2_float}')
-  elif operador == '*':
-    print(f'O resultado é: {num_1_float * num_2_float}')
-  elif operador == '/':
-    print(f'O resultado é: {num_1_float / num_2_float}')
-  else:
-    print('Nunca deveria chegar até aqui')
+    if operador == '+':
+        print(f'O resultado é: {num_1_float + num_2_float}')
+    elif operador == '-':
+        print(f'O resultado é: {num_1_float - num_2_float}')
+    elif operador == '*':
+        print(f'O resultado é: {num_1_float * num_2_float}')
+    elif operador == '/':
+        print(f'O resultado é: {num_1_float / num_2_float}')
+    else:
+        print('Nunca deveria chegar até aqui')
 
-  sair = input('Deseja sair agora? Pressione [s] para Sim ').lower().startswith('s') #você pode incluir vários métodos dentro de uma variável ao mesmo tempo.
+    sair = input('Deseja sair agora? Pressione [s] para Sim ').lower().startswith('s') #você pode incluir vários métodos dentro de uma variável ao mesmo tempo.
 
-  if sair is True:
-    print('Usuário saiu')
-    break
+    if sair is True:
+        print('Usuário saiu')
+        break
